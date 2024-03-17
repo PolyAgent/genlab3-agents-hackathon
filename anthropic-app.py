9384c8147a12e7f464671bbca24089839326f513
antp <anton.polishko@gmail.com>
2024-03-16 17:29:43 -0700

    add anthropic chat app

---
+++ anthropic-app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import anthropic
+
+client = anthropic.Client(api_key=st.secrets.anthropic.key)
+
+st.title("VC pilot Claude-3-opus")
+
+if question := st.chat_input("How risky is this project?:"):
+    st.chat_message("user").markdown(question)
+    prompt = f"""{anthropic.HUMAN_PROMPT} {question} {anthropic.AI_PROMPT}"""
+    print(prompt)
+
+
+    response = client.messages.create(
+        model="claude-3-opus-20240229",
+        max_tokens=1024,
+        messages=[
+            {"role": "user", "content": question}
+        ]
+    )
+    st.write("### Answer")
+    st.write(response.content[0].text)
